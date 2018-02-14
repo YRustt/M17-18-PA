0c0f564df09f17edb28b6049b46ca7348378cfe2
Rustam Yunusov <yrustt@yandex.ru>
2018-02-14 23:26:42 +0300

    Refactoring

--- Lab_1/main.c
+++ Lab_1/main.c
@@ -9,27 +9,35 @@
 #include <inttypes.h>
 
 
-typedef uint32_t T;
+uint32_t _round(const uint32_t a, const uint32_t b) {
+    return a / b + ((a % b) ? 1 : 0);
+}
+
+
+uint32_t _get_code(char s) {
+    if ((s >= '0') && (s <= '9')) return (uint32_t) (s - '0');
+    return (uint32_t) (s - 'A');
+}
+
+
+uint32_t _get_number(uint32_t a[8]) {
+    return (a[0] << 28) + (a[1] << 24) + (a[2] << 20) + (a[3] << 16) + (a[4] << 12) + (a[5] << 8) + (a[6] << 4) + a[7];
+}
 
 
 struct BinaryMatrix {
     uint32_t size, line_size;
     uint32_t sls_for_line, sls_for_T;
-    T* m;
+    uint32_t *m;
 };
-
-
-T _round(const uint32_t a, const uint32_t b) {
-    return a / b + ((a % b) ? 1 : 0);
-}
 
 
 bool create_BinaryMatrix(struct BinaryMatrix* binaryMatrix, uint32_t size) {
     binaryMatrix->size = size;
-    binaryMatrix->sls_for_T = sizeof(T) / 4;
+    binaryMatrix->sls_for_T = sizeof(uint32_t) / 4;
     binaryMatrix->sls_for_line = _round(binaryMatrix->size, 4);
     binaryMatrix->line_size = _round(binaryMatrix->sls_for_line, binaryMatrix->sls_for_T);
-    binaryMatrix->m = malloc(binaryMatrix->size * binaryMatrix->line_size * sizeof(T));
+    binaryMatrix->m = malloc(binaryMatrix->size * binaryMatrix->line_size * sizeof(uint32_t));
 
     return binaryMatrix->m;
 }
@@ -46,13 +54,22 @@ void delete_BinaryMatrix(struct BinaryMatrix* binaryMatrix) {
 
 
 bool init_BinaryMatrix(struct BinaryMatrix* binaryMatrix, FILE* file) {
-    uint8_t* buff = malloc(binaryMatrix->sls_for_line * sizeof(uint8_t));
+    char* buff = malloc(binaryMatrix->sls_for_line * sizeof(char));
 
     for (uint32_t i = 0; i < binaryMatrix->size; ++i) {
         fscanf(file, "%s", buff);
-        printf("%s\n", buff);
+
+        uint32_t a[8];
+
+        for (uint32_t j = 0; j < binaryMatrix->sls_for_line; j += 8) {
+            for (uint32_t k = 0; k < 8; ++k) {
+                a[k] = (j + k < binaryMatrix->sls_for_line)? _get_code(buff[j + k]) : 0;
+            }
+            binaryMatrix->m[i * binaryMatrix->line_size + j / 8] = _get_number(a);
+        }
     }
 
+    free(buff);
     return true;
 }
 
@@ -71,13 +88,19 @@ int main() {
     fscanf(file, "%" SCNu32, &n);
 
 
-    struct BinaryMatrix binaryMatrix;
-    create_BinaryMatrix(&binaryMatrix, 6);
+    struct BinaryMatrix A, B, C;
+    create_BinaryMatrix(&A, n);
+    create_BinaryMatrix(&B, n);
+    create_BinaryMatrix(&C, n);
 
-    init_BinaryMatrix(&binaryMatrix, file);
+    init_BinaryMatrix(&A, file);
+    init_BinaryMatrix(&B, file);
+    init_BinaryMatrix(&C, file);
 
 
-    delete_BinaryMatrix(&binaryMatrix);
+    delete_BinaryMatrix(&A);
+    delete_BinaryMatrix(&B);
+    delete_BinaryMatrix(&C);
 
     return 0;
 }
